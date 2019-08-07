c237a64e316d2900ec214895c07f5a2fa35eb5c4
Henrik <henrik.schulze@materna.de>
2019-08-07 10:35:04 +0200

    error in alicloud_vswitch

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "alicloud_vswitch" "this" {
   vpc_id            = alicloud_vpc.this.id
   cidr_block        = cidrsubnet(var.vpc_cidr_block, 10, each.key)
   name              = each.value
-  availability_zone = data.alicloud_zones.default.0.id
+  availability_zone = data.alicloud_zones.default.zones.0.id
   depends_on = [
     "alicloud_vpc.this"
   ]
